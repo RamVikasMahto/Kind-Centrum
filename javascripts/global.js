5d2c06f3d66ed0cacbbd06e5c000b1dbe78faf5f
Ram Vikas Mahto <vikas.mehto782@gmail.com>
2018-06-18 14:17:58 +0530

    project add

---
+++ javascripts/global.js
@@ -0,0 +1,23 @@
+	
+	 
+  $(document).ready(function () { 
+	
+	$(".mobileMenu").click(function(){
+    $(".menuBox").slideToggle();
+});
+	
+    }); 
+
+  $(document).ready(function () { 
+        $('body').sectionScroll(); // Easy Peasy Lemon Squeezy 
+    }) 
+
+			
+			
+		
+	$('.fancybox').fancybox();	
+			
+			
+		
+
+	
